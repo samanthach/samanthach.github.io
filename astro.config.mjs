06780ec5ad164b4b3cf2737eb0418c0accceebbf
Samantha Chu <samanthachu@Samanthas-Air.lan>
2024-09-09 16:33:15 -0400

    remove base setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    base: '/samanthach.github.io/',
     integrations: [
         mdx(),
         sitemap(),

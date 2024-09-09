c4383638b661114bce977be34d663d80fe92d7ba
Samantha Chu <samanthachu@Samanthas-Air.lan>
2024-09-09 16:31:06 -0400

    add base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+    base: '/samanthach.github.io/',
     integrations: [
         mdx(),
         sitemap(),
